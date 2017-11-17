84a1ef8c193d0ddf09657568fb1d45c860607000
Kannika Khanankhlai <biew_dd36@hotmail.com>
2017-11-17 11:14:13 +0700

    Successfully information data

--- src/oop_lab/s359211110141/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110141/rmutsv/com/myFirstJAVA.java
@@ -4,6 +4,9 @@ public class myFirstJAVA {
 
     public static void main(String[] args) {
         System.out.println("Hello Wordl, I'm Kannika");
+        System.out.println("Information System");
+        System.out.println("Faculty of Manggement Technolgy");
+        System.out.println("RMUTSV");
     } // main
 
 } // class
