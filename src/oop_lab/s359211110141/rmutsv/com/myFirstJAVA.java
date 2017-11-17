9b5e743f6e913efc9ded7cffe1145807df917c27
Kannika Khanankhlai <biew_dd36@hotmail.com>
2017-11-17 10:32:25 +0700

    Testing upload to GitHub

---
+++ src/oop_lab/s359211110141/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,9 @@
+package oop_lab.s359211110141.rmutsv.com;
+
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl, I'm Kannika");
+    } // main
+
+} // class
