365e0ef78c6ca02894eadf6bc1285d0feff7e8f4
LAB_COM-MT_1 <biew_dd36@hotmail.com>
2017-12-20 17:55:25 +0700

    Test StudentData

---
+++ src/ooplab4/StudentData.java
@@ -0,0 +1,21 @@
+package ooplab4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//1. ชื่อ-สกุล
+//2. รหัสนักศึกษา
+//3. สาขาที่เรียน และคณะ
+//4. ที่อยู่
+//5. email
+public class StudentData {
+    static BufferedReader reader=new BufferedReader(new InputStreamReader(System))
+    public static void main(String[] args) {
+        getName();
+        
+    }//main
+
+    private static void getName() {
+    }
+
+}//class
