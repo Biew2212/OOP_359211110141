d3c644aac33f7077f0d685b187ce6cc1c936479a
LAB_COM-MT_1 <biew_dd36@hotmail.com>
2017-12-22 11:34:20 +0700

    Test Exercise

---
+++ src/ooplab5/Exercise.java
@@ -0,0 +1,16 @@
+package ooplab5;
+//เขียนโปรแกรมรับค่าจำนวนเต็มจากผู้ใช้ 20 จำนวน จากนั้นหาข้อมูลดังต่อไปนี้
+//1.ผลรวมของข้อมูล
+//2.ค่าเฉลี่ย
+//3.ค่ามากที่สุด
+//4.ค่าน้อยที่สุด
+//5.แสดงข้อมูลโดยการเรียงจากน้อยไปมาก
+//6.แสดงข้อมูลโดยการเรียงจากมากไปน้อย
+public class Exercise {
+    private static int MAX = 20;
+    public static void main(String[] args) {
+        Integer[] num = new Integer[MAX];
+        num
+
+    }//main
+}//class
